fef6a68060cc0c606a106f49e8b5107cab223b52
Michael P. Soulier <msoulier@digitaltorque.ca>
2024-12-27 12:34:40 -0500

    Updated formatting, and made the report local

--- newyears.go
+++ newyears.go
@@ -8,9 +8,8 @@ import (
 
 func main() {
     now := time.Now().UTC()
-    fmt.Printf("%v\n", now)
-    newyears := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.UTC)
-    fmt.Printf("%v\n", newyears)
+    fmt.Printf("The time is now %s\n", now.Local())
+    newyears := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.Local)
     diff := newyears.Sub(now)
-    fmt.Printf("%s until %s\n", mlib.Duration2Human(diff), newyears)
+    fmt.Printf("It is %s until local New Years\n", mlib.Duration2Human(diff))
 }
