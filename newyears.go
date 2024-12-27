540abe94113b7de15309f0e270e3fe14da61cb72
Michael P. Soulier <msoulier@digitaltorque.ca>
2024-12-27 15:50:41 -0500

    Having a little fun.

--- newyears.go
+++ newyears.go
@@ -6,10 +6,36 @@ import (
     "github.com/msoulier/mlib"
 )
 
-func main() {
+var Reset  = "\033[0m"
+var Red    = "\033[31m"
+var Green  = "\033[32m"
+var Yellow = "\033[33m"
+var Blue   = "\033[34m"
+var Purple = "\033[35m"
+var Cyan   = "\033[36m"
+var Gray   = "\033[37m"
+var White  = "\033[97m"
+var width = 70
+
+func telltime() {
+    fmt.Printf("\r")
     now := time.Now().UTC()
-    fmt.Printf("The time is now %s\n", now.Local())
-    newyears := time.Date(2025, time.January, 1, 0, 0, 0, 0, time.Local)
+    year := now.Local().Year()
+    next_year := year+1
+    newyears := time.Date(next_year, time.January, 1, 0, 0, 0, 0, time.Local)
     diff := newyears.Sub(now)
-    fmt.Printf("It is %s until local New Years\n", mlib.Duration2Human(diff))
+    printstring := fmt.Sprintf("Countdown: %s until %d", mlib.Duration2Human(diff), next_year)
+    finalstring := printstring[:]
+    if len(printstring) > width {
+        finalstring = printstring[:width]
+    }
+    format := fmt.Sprintf("%%s%%%ds%%s", width)
+    fmt.Printf(format, Purple, finalstring, Reset)
+}
+
+func main() {
+    for {
+        telltime()
+        time.Sleep(1*time.Second)
+    }
 }
